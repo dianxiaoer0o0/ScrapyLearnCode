f2e11c36bc73b4cd61a8ea868549a14348516ab6
dianxiaoer <weizhixiong_92@126.com>
2018-11-27 12:08:09 +0800

    scrapy提取一组数据

--- Scrapy/mingyan/mingyan/spiders/mingyan_spider.py
+++ Scrapy/mingyan/mingyan/spiders/mingyan_spider.py
@@ -5,19 +5,23 @@ class mingyan(scrapy.Spider):
 
     name = "mingyan"
     start_urls = [
-        'http://lab.scrapyd.cn/page/1/',
-        'http://lab.scrapyd.cn/page/2/',
+        'http://lab.scrapyd.cn',
     ]
     def parse(self,response):
-        mingyan = response.css('div.quote')[0]
+        mingyan = response.css('div.quote')
+        for item in mingyan:
+            text = item.css('.text::text').extract_first()
+            author = item.css('.author::text').extract()
+            tags = item.css('.tags .tag::text').extract()
+            tags = ",".join(tags)
 
-        text = mingyan.css('.text::text').extract_first()
-        author = mingyan.css('.author::text').extract()
-        tags = mingyan.css('.tags .tag::text').extract()[0:2]
-        tags = ",".join(tags)
-
-        fileName = '%s-语录.txt' % author
-        with open(fileName,'w') as f:
-            f.write(text)
-            f.write('\n')
-            f.write('标签：' + tags)
+            fileName = '%s-语录.txt' % author[0]
+            with open(fileName,'a+') as f:
+                f.write(text)
+                f.write('\n')
+                f.write('标签：' + tags)
+                f.write('\n---------------------\n')
+        nextPage = response.css('li.next a::attr(href)').extract_first()
+        if nextPage is not None:
+            nextPage = response.urljoin(nextPage)
+            yield scrapy.Request(nextPage,callback=self.parse)
